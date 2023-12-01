7d85cdc10fa066148d5db4fe5779203d263d2475
Rohan-Mainali <rohanmainali39@gmail.com>
2023-12-01 13:54:47 +0545

    feat: simple get API using gin

--- main.go
+++ main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type albumn struct {
+	ID     string  `json : "id"`
+	Title  string  `json : "title"`
+	Artist string  `json : "artist"`
+	Price  float64 `json : "float"`
+}
+
+var albumns = []albumn{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func getAlbumns(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albumns)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albumns", getAlbumns)
+
+	router.Run("localhost:8080")
+}
